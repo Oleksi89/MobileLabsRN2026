f4cb1e86d97c92e639d0170850964b79928e6eed
Oleksi89 <derdef360@gmail.com>
2026-04-01 15:38:10 +0300

    feat: implement news list with FlatList and temp App connection

--- lab2/App.js
+++ lab2/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import MainScreen from "./src/screens/MainScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <SafeAreaView style={{ flex: 1 }}>
+    <StatusBar style="dark"/>
+    <MainScreen />
+  </SafeAreaView>
   );
 }
 
