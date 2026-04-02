9f971b7baf4f2142b5706b8896aea283961932cf
Oleksi89 <derdef360@gmail.com>
2026-04-02 12:58:17 +0300

    feat: add bottom tab navigation and connect context providers

--- lab3/App.js
+++ lab3/App.js
@@ -1,20 +1,20 @@
+import 'react-native-gesture-handler';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { CustomThemeProvider } from './src/context/ThemeContext';
+import { GameProvider } from './src/context/GameContext';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <StatusBar style="dark" />
+        <CustomThemeProvider>
+          <GameProvider>
+            <AppNavigator />
+          </GameProvider>
+        </CustomThemeProvider>
+      </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
